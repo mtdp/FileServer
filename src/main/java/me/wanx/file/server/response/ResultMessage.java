dfb828a43148ecf99ca031141eba9faa1d59f748
sun-wanx <594566751@qq.com>
2014-11-14 11:35:58 +0800

    测试修改提交

--- src/main/java/me/wanx/file/server/response/ResultMessage.java
+++ src/main/java/me/wanx/file/server/response/ResultMessage.java
@@ -2,6 +2,14 @@ package me.wanx.file.server.response;
 
 import java.io.Serializable;
 
+/**
+ * 
+* @ClassName: ResultMessage 
+* @Description: TODO 
+* @author gqwang
+* @date 2014年11月14日 上午11:54:48 
+*
+ */
 public class ResultMessage implements Serializable {
 	private static final long serialVersionUID = -2753307512189953499L;
 	
